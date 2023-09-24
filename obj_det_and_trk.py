2bc6538c598a08a4aa2dc1364505db072f8c072a
tahanaveed97 <tahanaveed@hotmail.com>
2023-09-24 12:44:08 +0500

    tracking plus in/out counting implemented

--- obj_det_and_trk.py
+++ obj_det_and_trk.py
@@ -14,43 +14,12 @@ from utils.torch_utils import select_device, time_sync
 from sort import *
 
 
-
-palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
-def compute_color_for_labels(label):
-    color = [int(int(p * (label ** 2 - label + 1)) % 255) for p in palette]
-    return tuple(color)
+def ccw(A,B,C):
+    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
 
 
-"""Function to Draw Bounding boxes"""
-def draw_boxes(img, bbox, identities=None, categories=None, 
-                names=None, color_box=None,offset=(0, 0)):
-    for i, box in enumerate(bbox):
-        x1, y1, x2, y2 = [int(i) for i in box]
-        x1 += offset[0]
-        x2 += offset[0]
-        y1 += offset[1]
-        y2 += offset[1]
-        cat = int(categories[i]) if categories is not None else 0
-        id = int(identities[i]) if identities is not None else 0
-        data = (int((box[0]+box[2])/2),(int((box[1]+box[3])/2)))
-        label = str(id)
-
-        if color_box:
-            color = compute_color_for_labels(id)
-            (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
-            cv2.rectangle(img, (x1, y1), (x2, y2),color, 2)
-            cv2.rectangle(img, (x1, y1 - 20), (x1 + w, y1), (255,191,0), -1)
-            cv2.putText(img, label, (x1, y1 - 5),cv2.FONT_HERSHEY_SIMPLEX, 0.6, 
-            [255, 255, 255], 1)
-            cv2.circle(img, data, 3, color,-1)
-        else:
-            (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
-            cv2.rectangle(img, (x1, y1), (x2, y2),(255,191,0), 2)
-            cv2.rectangle(img, (x1, y1 - 20), (x1 + w, y1), (255,191,0), -1)
-            cv2.putText(img, label, (x1, y1 - 5),cv2.FONT_HERSHEY_SIMPLEX, 0.6, 
-            [255, 255, 255], 1)
-            cv2.circle(img, data, 3, (255,191,0),-1)
-    return img
+def intersect(A,B,C,D):
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
 
 def detect():
@@ -61,6 +30,8 @@ def detect():
     device='cpu'  
     conf_thres = 0.25
     half = False
+
+    memory = {}
 
     #.... Initialize SORT .... 
     sort_max_age = 5 
@@ -85,7 +56,18 @@ def detect():
 
     cap = cv2.VideoCapture('2.mp4')
 
+    line_position = 240
+
+    roi_line = [(0, line_position), (640, line_position)]
+    line = [roi_line[0], roi_line[1]]
+    intersect_val = int(line_position)
+
+    in_count = 0
+    out_count = 0
+
+
     while True:
+
         ret, im0 = cap.read()
         if im0 is None:
             cap = cv2.VideoCapture('2.mp4')
@@ -98,14 +80,16 @@ def detect():
             im = im[None]  # expand for batch dim
 
         # Inference
-
         pred = model(im, augment=False, visualize=False)
 
         # NMS
         pred = non_max_suppression(pred, conf_thres, iou_thres, [0], False, max_det=max_det)
 
+        previous = memory.copy()
+        boxes, indexIDs, memory = [], [], {}
 
-        for i, det in enumerate(pred):  # per image
+        for i, det in enumerate(pred):
+
             if len(det):
                 det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
 
@@ -118,22 +102,39 @@ def detect():
                 
                 # Run SORT
                 tracked_dets = sort_tracker.update(dets_to_sort)
-                tracks =sort_tracker.getTrackers()
-                
 
-                #loop over tracks
-                for track in tracks:
-                    [cv2.line(im0, (int(track.centroidarr[i][0]),int(track.centroidarr[i][1])), 
-                            (int(track.centroidarr[i+1][0]),int(track.centroidarr[i+1][1])),
-                            (124, 252, 0), thickness=3) for i,_ in  enumerate(track.centroidarr) 
-                            if i < len(track.centroidarr)-1 ] 
-                
-                # draw boxes for visualization
-                if len(tracked_dets)>0:
-                    bbox_xyxy = tracked_dets[:,:4]
-                    identities = tracked_dets[:, 8]
-                    categories = tracked_dets[:, 4]
-                    draw_boxes(im0, bbox_xyxy, identities, categories, names, None)
+                for track in tracked_dets:
+                    boxes.append([track[0], track[1], track[2], track[3]])
+                    indexIDs.append(int(track[8]))
+                    memory[indexIDs[-1]] = boxes[-1]
+
+                if len(boxes) > 0:
+                    for i, box in enumerate(boxes):
+                        (x, y) = (int(box[0]), int(box[1]))
+                        (w, h) = (int(box[2]), int(box[3]))
+                        cv2.rectangle(im0, (x, y), (w, h), (255, 0, 0), 2)
+                        if indexIDs[i] in previous:
+                            previous_box = previous[indexIDs[i]]
+                            (x2, y2) = (int(previous_box[0]), int(previous_box[1]))
+                            (w2, h2) = (int(previous_box[2]), int(previous_box[3]))
+                            p0 = (int(x + (w - x) / 2), int(y + (h - y) / 2))
+                            p1 = (int(x2 + (w2 - x2) / 2), int(y2 + (h2 - y2) / 2))
+                            im0 = cv2.line(im0, p0, p1, (0,0,255), 4)
+
+                            if intersect(p0, p1, line[0], line[1]):
+                                if p0[1] > intersect_val:
+                                    in_count+=1
+
+                                elif p0[1] < intersect_val:
+                                    out_count+=1
+
+                        text = "{}".format(indexIDs[i])
+                        cv2.putText(im0, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 1)
+
+        cv2.line(im0, line[0], line[1], (255, 49, 255), 5)
+        cv2.putText(im0, "In Count" + str(in_count), (20, 100), 2, 1, (0, 0, 255), 2)
+        cv2.putText(im0, "Out Count" + str(out_count), (20, 150), 2, 1, (0, 0, 255), 2)
+
         cv2.imshow('img', im0)
         cv2.waitKey(1)
 
